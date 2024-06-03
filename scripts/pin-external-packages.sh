3b9979240ae7ac497c679de0c59d1fbb510eee1f
Robin Bate Boerop <me@robinbb.com>
2024-06-03 17:30:26 +0000

    Avoid faulty success on build

--- scripts/pin-external-packages.sh
+++ scripts/pin-external-packages.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
+#! /usr/bin/env bash
+
+set -x
+set -euo pipefail
 
 # update and pin packages, used by CI
 
 PACKAGES="ocaml-sodium capnp-ocaml rpc_parallel async_kernel coda_base58"
 
-git submodule sync && git submodule update --init --recursive
+git submodule sync
+git submodule update --init --recursive
 
 for pkg in $PACKAGES; do
-    echo "Pinning package" $pkg
-    opam pin -y add src/external/$pkg
+    echo "Pinning package: $pkg..."
+    opam pin --switch=default -y add src/external/$pkg
 done
